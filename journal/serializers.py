e7effd1cc36832ef22450a5001bb8cb72ca1690e
diegosouzacosta <diego.de.scosta@gmail.com>
2015-07-30 17:56:49 -0300

    add urls.py na app journal, configurada estrutura rest

--- journal/serializers.py
+++ journal/serializers.py
@@ -3,22 +3,24 @@
 from rest_framework import serializers
 from rest_framework.pagination import PageNumberPagination
 
+from journal.models import Projects
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     """
-    Serialize the model Project
+    Serialize the model Projects
     """
     class Meta:
-        model = Project
+        model = Projects
         fields = (
-                'id',
+                'id', 'github_id', 'name', 'description', 'html_url', 'created_at',
         )
 
 
 class ProjectPagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
-    max_page_size = 10
+    max_page_size = 10000000
 
     class Meta:
         object_serializer_class = ProjectSerializer
--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from rest_framework import generics
+
+import logging
+from journal.models import Projects
+from journal.serializers import ProjectSerializer
+from journal.serializers import ProjectPagination
+
+
+log = logging.getLogger(__name__)
+
+
+class JournalViewSet(generics.ListAPIView):
+    '''
+    - View that returns the query to the model Projec. Send in JSON format
+    '''
+    # disable interface of django-rest-framework
+    # renderer_classes = [renderers.JSONRenderer]
+
+    queryset = Projects.objects.all()
+    serializer_class = ProjectSerializer
+    pagination_class = ProjectPagination
+    model = Projects
