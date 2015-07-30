6e0ec8f59f8405f96512fc43847cf80c26844dbc
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-30 17:02:27 -0300

    webhooks urls and views

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class PingView(APIView):
+    '''
+    Endpoint to receive github requests.
+    '''
+    def get(self, request, format=None):
+        return Response({'ping?': 'pong!'}, status=status.HTTP_201_CREATED)
+
+
+class ReceiveRequestsView(APIView):
+    '''
+    Endpoint to receive github requests.
+    '''
+    def get(self, request, format=None):
+        return Response({}, status=status.HTTP_201_CREATED)
+
+    def post(self, request, format=None):
+        print(1)
+        # TODO
+        return Response({}, status=status.HTTP_201_CREATED)
