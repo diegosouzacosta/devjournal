fd4f67c6a69cdccf8a39110cb5c5664f497987ed
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-30 16:27:15 -0300

    webhooks include urls

--- devjournal/urls.py
+++ devjournal/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^webhooks/', include('webhooks.urls', namespace='webhooks')),
     url(r'^admin/', include(admin.site.urls)),
 ]
