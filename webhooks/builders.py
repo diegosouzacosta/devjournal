57f7e6ce2e2262c80e95c3cf9306495723f5485a
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-31 07:41:28 -0300

    test fix

--- webhooks/builders.py
+++ webhooks/builders.py
@@ -103,30 +103,35 @@ def developer_builder(json_object):
     return developer
 
 
-def milestone_builder(json_object, project):
+def milestone_builder(json_object, project, developer):
     creator = developer_builder(json_object['creator'])
     milestone = Milestone.objects.filter(github_id=json_object['id'],).last()
 
-    if not milestone:
-        return Milestone.objects.create(
-            github_id=json_object['id'],
+    if milestone:
+        milestone.github_id = json_object.get('id')
+        milestone.number = json_object.get('number')
+        milestone.state = json_object.get('state')
+        milestone.title = json_object.get('title')
+        milestone.description = json_object.get('description')
+        milestone.html_url = json_object.get('html_url')
+        milestone.created_at = json_object.get('created_at')
+        milestone.closed_at = json_object.get('closed_at')
+        milestone.updated_at = json_object.get('updated_at')
+        milestone.due_on = json_object.get('due_on')
+        milestone.project = project
+        milestone.creator = developer
 
-            number=json_object['number'],
-            state=json_object['state'],
-            title=json_object['title'],
-            description=json_object['description'],
-            html_url=json_object['html_url'],
-            sender=json_object['sender'],
-
-            create_at=json_object['create_at'],
-            closed_at=json_object['chosed_at'],
-            due_on=json_object['due_on'],
-            updated_at=json_object['updated_at'],
-
-            project=project,
-            creator=creator,
-        )
-
-    milestone.title
-    milestone.state
-    milestone.description
+    return Milestone.objects.create(
+        github_id=json_object['id'],
+        number=json_object['number'],
+        state=json_object['state'],
+        title=json_object['title'],
+        description=json_object['description'],
+        html_url=json_object['html_url'],
+        created_at=json_object['created_at'],
+        closed_at=json_object['closed_at'],
+        due_on=json_object['due_on'],
+        updated_at=json_object['updated_at'],
+        project=project,
+        creator=creator,
+    )
