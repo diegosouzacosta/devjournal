ec0a94632698f3f43292250dbba91535373052e3
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-31 05:31:55 -0300

    webhooks

--- webhooks/builders.py
+++ webhooks/builders.py
@@ -85,6 +85,30 @@ def project_builder(json_object):
     )
 
 
+def milestone_builder(json_object):
+    github_id = json_object.get('id')
+    project = Project.objects.filter(github_id=github_id).first()
+
+    if project:
+        project.github_id = json_object.get('id')
+        project.name = json_object.get('name')
+        project.description = json_object.get('description')
+        project.html_url = json_object.get('url')
+        project.created_at = json_object.get('created_at')
+        project.creator = developer_builder(json_object.get('owner'))
+        project.save()
+        return project
+
+    return Project.objects.create(
+        github_id=json_object.get('id'),
+        name=json_object.get('name'),
+        description=json_object.get('description'),
+        html_url=json_object.get('url'),
+        created_at=json_object.get('created_at'),
+        creator=developer_builder(json_object.get('owner'))
+    )
+
+
 def developer_builder(json_object):
     developer, __ = Developer.objects.get_or_create(
         github_id=json_object['id'],
