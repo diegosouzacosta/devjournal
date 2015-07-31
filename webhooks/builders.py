a663da21c4f14b0ebefa7e9dc867455e1d91b351
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-31 04:19:53 -0300

    code #15: initial code

--- webhooks/builders.py
+++ webhooks/builders.py
@@ -61,6 +61,28 @@ def organization_builder(json_object):
     )
 
 
+def project_builder(json_object):
+    github_id = json_object.get('id')
+    project = Organization.objects.filter(github_id=github_id).first()
+
+    if project:
+        project.github_id = json_object.get('id')
+        project.name = json_object.get('login')
+        project.description = json_object.get('description')
+        project.html_url = json_object.get('url')
+        project.avatar_url = json_object.get('avatar_url')
+        project.save()
+        return project
+
+    return Organization.objects.create(
+        github_id=json_object.get('id'),
+        name=json_object.get('login'),
+        description=json_object.get('description'),
+        html_url=json_object.get('url'),
+        avatar_url=json_object.get('avatar_url')
+    )
+
+
 def developer_builder(json_object):
     developer, __ = Developer.objects.get_or_create(
         github_id=json_object['id'],
