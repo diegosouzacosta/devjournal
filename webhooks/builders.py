857d2a6ca03161980b472baeaa06a568551ca372
Augusto F. Hack <hack.augusto@gmail.com>
2015-07-31 07:57:02 -0300

    issue builder

--- webhooks/builders.py
+++ webhooks/builders.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from journal.models import Developer, Organization, Label, Project, Milestone
+from journal.models import Developer, Organization, Label, Project, Milestone, Issue
 
 
 ISSUE = 'issue'
@@ -11,30 +11,43 @@ def builder(github_event, json_object):
         issue_builder(json_object)
 
 
-def issue_builder(json_object):
-    pass
+def issue_builder(json_object, milestone, developer):
+    creator = developer_builder(json_object['creator'])
+    assignee = developer_builder(json_object['assignee'])
+    milestone = milestone_builder(json_object['milestone'])
+
+    return Issue.objects.create(
+        github_id=json_object['id'],
+        number=json_object['number'],
+        html_url=json_object['html_url'],
+        closed_at=json_object['closed_at'],
+        created_at=json_object['created_at'],
+        updated_at=json_object['updated_at'],
+        title=json_object['title'],
+        body=json_object['body'],
+        state=json_object['state'],
+        creator=creator,
+        assignee=assignee,
+        milestone=milestone,
+    )
 
 
 def label_builder(json_object, project):
     name = json_object['name']
     color = json_object['color']
 
-    label, __ = Label.objects.get_or_create(
-        name=name,
-        project=project,
-        defaults={'color': color},
-    )
+    label = Label.objects.filter(name=name, project=project).last()
 
-    return label_create_if_changed(label, json_object, project)
-
-
-def label_create_if_changed(label, json_object, project):
-    if label.color != json_object['color']:
+    if not label:
         return Label.objects.create(
-            name=json_object['name'],
+            name=name,
+            color=color,
             project=project,
-            color=json_object['color'],
         )
+
+    if label.color != color:
+        label.color = json_object['color']
+        label.save()
 
     return label
 
@@ -65,40 +78,48 @@ def project_builder(json_object, organization):
     github_id = json_object.get('id')
     project = Project.objects.filter(github_id=github_id).first()
 
-    if project:
-        project.github_id = json_object.get('id')
-        project.name = json_object.get('name')
-        project.description = json_object.get('description')
-        project.html_url = json_object.get('url')
-        project.created_at = json_object.get('created_at')
-        project.creator = developer_builder(json_object.get('owner'))
-        project.organization = organization
-        project.save()
+    if not project:
+        project = Project.objects.create(
+            github_id=json_object.get('id'),
+            name=json_object.get('name'),
+            description=json_object.get('description'),
+            html_url=json_object.get('url'),
+            created_at=json_object.get('created_at'),
+            creator=developer_builder(json_object.get('owner')),
+            organization=organization,
+        )
         return project
 
-    return Project.objects.create(
-        github_id=json_object.get('id'),
-        name=json_object.get('name'),
-        description=json_object.get('description'),
-        html_url=json_object.get('url'),
-        created_at=json_object.get('created_at'),
-        creator=developer_builder(json_object.get('owner')),
-        organization=organization,
-    )
+    project.github_id = json_object.get('id')
+    project.name = json_object.get('name')
+    project.description = json_object.get('description')
+    project.html_url = json_object.get('url')
+    project.created_at = json_object.get('created_at')
+    project.creator = developer_builder(json_object.get('owner'))
+    project.organization = organization
+    project.save()
+
+    return project
 
 
 def developer_builder(json_object):
-    developer, __ = Developer.objects.get_or_create(
-        github_id=json_object['id'],
-        defaults={
-            'avatar_url': json_object['avatar_url'],
-            'github_login': json_object['login'],
-        }
-    )
+    github_id = json_object['id']
+    avatar_url = json_object['avatar_url']
+    github_login = json_object['login']
 
-    if developer.avatar_url != json_object['avatar_url']:
-        developer.avatar_url = json_object['avatar_url']
-        developer.save()
+    developer = Developer.objects.filter(github_id=github_id).last()
+
+    if not developer:
+        return Developer.objects.create(
+            github_id=github_id,
+            avatar_url=avatar_url,
+            github_login=github_login,
+
+        )
+
+    developer.avatar_url = avatar_url
+    developer.github_login = github_login
+    developer.save()
 
     return developer
 
