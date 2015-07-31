06c16bb4127352f86633e2df9d436d291dba4c42
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-31 06:02:11 -0300

    close #15: test fix

--- webhooks/builders.py
+++ webhooks/builders.py
@@ -55,7 +55,7 @@ def organization_builder(json_object):
     return Organization.objects.create(
         github_id=json_object.get('id'),
         name=json_object.get('login'),
-        description=json_object.get('description'),
+            description=json_object.get('description'),
         html_url=json_object.get('url'),
         avatar_url=json_object.get('avatar_url')
     )
@@ -84,30 +84,6 @@ def project_builder(json_object, organization):
         created_at=json_object.get('created_at'),
         creator=developer_builder(json_object.get('owner')),
         organization=organization,
-    )
-
-
-def milestone_builder(json_object):
-    github_id = json_object.get('id')
-    project = Project.objects.filter(github_id=github_id).first()
-
-    if project:
-        project.github_id = json_object.get('id')
-        project.name = json_object.get('name')
-        project.description = json_object.get('description')
-        project.html_url = json_object.get('url')
-        project.created_at = json_object.get('created_at')
-        project.creator = developer_builder(json_object.get('owner'))
-        project.save()
-        return project
-
-    return Project.objects.create(
-        github_id=json_object.get('id'),
-        name=json_object.get('name'),
-        description=json_object.get('description'),
-        html_url=json_object.get('url'),
-        created_at=json_object.get('created_at'),
-        creator=developer_builder(json_object.get('owner'))
     )
 
 
--- webhooks/tests/test_project_builder.py
+++ webhooks/tests/test_project_builder.py
@@ -8,23 +8,40 @@ from journal.models import Project
 from webhooks.builders import project_builder, organization_builder
 
 
+organization = organization_builder({
+    "login": "github",
+    "id": 1,
+    "url": "https://api.github.com/orgs/github",
+    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+    "description": "A great organization"
+})
+
+
 class ProjectBuilderTestCase(TestCase):
     def test_project_builder_new_project(self):
-        organization = organization_builder({
-            "login": "github",
-            "id": 1,
-            "url": "https://api.github.com/orgs/github",
-            "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-            "description": "A great organization"
-        })
-
         count = Project.objects.count()
-
         with open('webhooks/tests/data/project.json') as json_file:
             project_json = json.load(json_file)
 
         project = project_builder(project_json, organization)
         self.assertEqual(Project.objects.count(), count + 1)
+        self.assertEqual(project.github_id, project_json.get('id'))
+        self.assertEqual(project.name, project_json.get('name'))
+        self.assertEqual(project.description, project_json.get('description'))
+        self.assertEqual(project.html_url, project_json.get('url'))
+        self.assertEqual(project.created_at, project_json.get('created_at'))
+
+    def test_project_builder_old_project(self):
+        count = Project.objects.count()
+        with open('webhooks/tests/data/project.json') as json_file:
+            project_json = json.load(json_file)
+
+        project = project_builder(project_json, organization)
+        self.assertEqual(Project.objects.count(), count + 1)
+        count = Project.objects.count()
+
+        project = project_builder(project_json, organization)
+        self.assertEqual(Project.objects.count(), count)
         self.assertEqual(project.github_id, project_json.get('id'))
         self.assertEqual(project.name, project_json.get('name'))
         self.assertEqual(project.description, project_json.get('description'))
