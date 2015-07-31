df205e1eed9313fe19a9699600a048708febd1f7
diegosouzacosta <diego.de.scosta@gmail.com>
2015-07-31 10:30:43 -0300

    corrigdo erro de distinct para issues, add weekly

--- journal/managers.py
+++ journal/managers.py
@@ -69,3 +69,69 @@ class DeveloperManager(models.Manager):
             organization__projects__milestones__issues__sender=F('pk'),
             organization=organization,
         ).get_extras_daily(date_start, date_end)
+
+
+class ProjectQuerySet(models.QuerySet):
+
+    def get_extras_daily(self, date_start, date_end):
+        '''Returns:
+        Get values for Organization building by time
+        '''
+        return self.annotate(
+            issues_closed=Count(
+                Case(
+                    When(
+                        milestones__issues__closed_at__gte=date_start,
+                        milestones__issues__closed_at__lte=date_end,
+                        then=1,
+                    )
+                ),
+            ),
+            issues_open=Count(
+                Case(
+                    When(
+                        milestones__issues__created_at__gte=date_start,
+                        milestones__issues__created_at__lte=date_end,
+                        then=1,
+                    )
+                ),
+            ),
+            issues_progress=Count(
+                Case(
+                    When(
+                        milestones__issues__label__name__icontains='progress',
+                        then=1,
+                    )
+                ),
+            ),
+            milestone_open=Count(
+                Case(
+                    When(
+                        milestones__state__icontains='open',
+                        then=1,
+                    )
+                ),
+            ),
+            milestone_percent=Count(
+                Case(
+                    When(
+                        milestones__issues__state__icontains='close',
+                        then=1,
+                    )
+                ),
+            ) / Count(F('milestones__issues')),
+        )
+
+
+class ProjectManager(models.Manager):
+
+    def get_queryset(self):
+        return ProjectQuerySet(self.model, using=self._db)
+
+    def get_values_weekly(self, date_start, date_end, organization):
+        return self.get_queryset().filter(
+            milestones__issues__updated_at__gte=date_start,
+            milestones__issues__updated_at__lte=date_end,
+            milestones__issues__sender=F('pk'),
+            organization=organization,
+        ).get_extras_daily(date_start, date_end)
--- journal/urls.py
+++ journal/urls.py
@@ -2,10 +2,12 @@
 
 from django.conf.urls import patterns, url
 
-from journal.views import JournalDailyViewSet
+from journal.views import JournalDailyViewSet, JournalWeeklyViewSet
 
 
 urlpatterns = patterns(
     'journal.views',
     url(r'^daily/$', JournalDailyViewSet.as_view()),
+    url(r'^weekly/$', JournalWeeklyViewSet.as_view()),
+
 )
