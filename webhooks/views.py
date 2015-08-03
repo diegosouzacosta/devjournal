1ab19c2c851a87e27d8398228703f290d6218a00
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-08-03 18:36:05 -0300

    changes

--- webhooks/views.py
+++ webhooks/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from webhooks.builder import builder
+
 
 class PingView(APIView):
     '''
